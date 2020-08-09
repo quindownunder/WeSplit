33badc6c7a6eba6c38ee89de8441746406f23043
Matthew Richardson <mat_richardson@mac.com>
2020-08-09 18:12:57 +1000

    Initial Version

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,11 +9,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            TextField("Enter your name", text: $name)
+            Text("Your name is \(name)")
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
