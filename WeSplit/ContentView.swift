e2910aa8558259609b41f82218d7bce1e4cec833
Matthew Richardson <mat_richardson@mac.com>
2020-08-09 18:23:37 +1000

    Views in a loop

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,15 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = 0
 
     var body: some View {
-        Form {
-            TextField("Enter your name", text: $name)
-            Text("Your name is \(name)")
+        VStack {
+            Picker("Select your student", selection: $selectedStudent) {
+                ForEach(0 ..< students.count) {
+                    Text(self.students[$0])
+                }
+            }
+            Text("You chose: Student # \(students[selectedStudent])")
         }
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
