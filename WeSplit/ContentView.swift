fa316fdb85f6239358691255b8c31160e5988440
Matthew Richardson <mat_richardson@mac.com>
2020-08-09 18:39:19 +1000

    Completed Project

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,18 +8,61 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = 0
+import SwiftUI
 
+struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    //computd propert
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        //nil coalescing operator
+        let orderAmount = Double(checkAmount) ?? 0
+
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Picker("Select your student", selection: $selectedStudent) {
-                ForEach(0 ..< students.count) {
-                    Text(self.students[$0])
-                }
+        NavigationView {
+            Form {
+                    Section {
+                        TextField("Amount", text: $checkAmount)
+                            .keyboardType(.decimalPad)
+                        
+                        Picker("Number of people", selection: $numberOfPeople) {
+                            ForEach(2 ..< 100) {
+                                Text("\($0) people")
+                            }
+                        }
+                    }
+                
+                    //segmented picker
+                    Section(header: Text("How much tip do you want to leave?")) {
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(0 ..< tipPercentages.count) {
+                                Text("\(self.tipPercentages[$0])%")
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                    
+                    Section {
+                        
+                        // c style operator https://en.wikipedia.org/wiki/Printf_format_string
+                        Text("$\(totalPerPerson, specifier: "%.2f")")
+                        
+                    }
             }
-            Text("You chose: Student # \(students[selectedStudent])")
+        .navigationBarTitle("WeSplit")
         }
     }
 }
